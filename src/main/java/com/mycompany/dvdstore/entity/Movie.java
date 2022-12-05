1ee46eff99e2a147f9a4ec28e1171a08456503ef
el mehdi <elmehdi.tailassan.ext@eulerhermes.com>
2022-12-05 21:14:42 +0100

    adding controllers

--- src/main/java/com/mycompany/dvdstore/entity/Movie.java
+++ src/main/java/com/mycompany/dvdstore/entity/Movie.java
@@ -6,12 +6,34 @@ public class Movie {
 
     private String genre;
 
+    private String description;
+
+    private Long id;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
     public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getGenre() {
